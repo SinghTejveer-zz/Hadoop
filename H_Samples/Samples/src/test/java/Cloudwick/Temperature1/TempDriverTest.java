a18d227fbe29d4ac2391b31925c5fc1d0831f2fa
teejay89 <tejveer@live.in>
2015-07-11 17:38:52 +0100

    Updated Code

--- H_Samples/Samples/src/test/java/Cloudwick/Temperature1/TempDriverTest.java
+++ H_Samples/Samples/src/test/java/Cloudwick/Temperature1/TempDriverTest.java
@@ -1,6 +1,7 @@
 package Cloudwick.Temperature1;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -29,11 +30,50 @@ public class TempDriverTest {
   }
 
   @Test
+  // Test for the 1st Line in the Input File
   public void testMapper() throws IOException {
     mapDriver.withInput(new LongWritable(), new Text(
         "  1853   1    8.4     2.7       4    62.8     ---"));
     mapDriver.withOutput(new Text("1853"), new DoubleWritable(8.4));
     mapDriver.runTest();
+  }
+
+  @Test
+  // Test for last line in the input data
+  public void testMapper_normalrec_normal() throws IOException {
+    mapDriver.withInput(new LongWritable(), new Text(
+        "   2014  12    8.7     2.4       9    44.0    96.9 "));
+    mapDriver.withOutput(new Text("2014"), new DoubleWritable(8.7));
+    mapDriver.runTest();
+  }
+
+  @Test
+  // Test for malformed data in the input data
+  public void testMapper_malformeddata_validmap() throws IOException {
+    mapDriver.withInput(new LongWritable(), new Text(
+        "   2014  12    ?     2.4       9    44.0    96.9 "));
+    mapDriver.runTest();
+  }
+
+  @Test
+  public void testReducer_validinput_validoutput() throws IOException {
+    ArrayList<DoubleWritable> values = new ArrayList<DoubleWritable>();
+    values.add(new DoubleWritable(6));
+    values.add(new DoubleWritable(8.7));
+    reduceDriver.withInput(new Text("2014"), values);
+    reduceDriver.withOutput(new Text("2014"), new DoubleWritable(8.7));
+    reduceDriver.runTest();
+  }
+
+  @Test
+  public void testMapReduce_validinput_validoutput() throws IOException {
+    mapReduceDriver.withInput(new LongWritable(), new Text(
+        "   2014  12    8.7     2.4       9    44.0    96.9 "));
+    ArrayList<DoubleWritable> values = new ArrayList<DoubleWritable>();
+    values.add(new DoubleWritable(8.7));
+    values.add(new DoubleWritable(6));
+    mapReduceDriver.withOutput(new Text("2014"), new DoubleWritable(8.7));
+    mapReduceDriver.runTest();
   }
 }
 
